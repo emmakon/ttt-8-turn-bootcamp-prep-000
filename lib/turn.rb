7c6156301bfb5e85625e04b7182d081db8dc8e1a
Emma Konrad <emma.konrad92@gmail.com>
2017-10-01 23:26:03 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -17,7 +17,7 @@ end
 
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == "   " || board[index] == nil
-    return false
+    false
   else
     return true
   end
